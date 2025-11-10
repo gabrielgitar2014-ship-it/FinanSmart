6a4b5a386b139fb3da4f3bcc26221dd788fde28d
gabrielgitar2014-ship-it <gabriel.gitar2014@gmail.com>
2025-11-10 12:17:29 -0300

    Delete src directory

--- src/components/icons/MastercardLogo.jsx
+++
@@ -1,9 +0,0 @@
-export default function MastercardLogo({ className }) {
-  return (
-    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 200">
-      <circle cx="100" cy="100" r="100" fill="#EA001B" />
-      <circle cx="220" cy="100" r="100" fill="#F79E1B" />
-      <path d="M160 100a100 100 0 0 1 0-100a100 100 0 0 1 0 200a100 100 0 0 0 0-200" fill="#FF5F00" />
-    </svg>
-  );
-}
--- src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Você encontra essas chaves no seu painel do Supabase
-const supabaseUrl = 'https://lboqmzbgiqinocdmoupv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxib3FtemJnaXFpbm9jZG1vdXB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MzQxMTIsImV4cCI6MjA3ODIxMDExMn0.E6VUWSQ67hPUBkAQt5SZTsR1ymnSk_mM7MrdTm-Jfm0'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
